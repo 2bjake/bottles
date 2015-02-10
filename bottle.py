ff39d3a1bbf052a1be9b085800ede702901bb093
2bjake <fosterjake@gmail.com>
2015-02-09 21:50:48 -0800

    support multiple bottles, liquid colors

--- bottle.py
+++ bottle.py
@@ -1,14 +1,20 @@
 class Bottle:
     """Bottle which can be filled with liquid """
-    def __init__(self, height, width, percent_filled=0):
-        self.height = height
+    def __init__(self, width, height, percent_filled=0, liquid_color=(0, 0, 255)):
         self.width = width
+        self.height = height
         self.percent_filled = percent_filled
+        self.liquid_color = liquid_color
 
-    def liquid_height(self):
+    def get_liquid_height(self):
         return self.height * self.percent_filled / 100
 
+    liquid_height = property(get_liquid_height)
+
     #TODO: concurrency issues
+    #TODO: adjusting the percentage is a hack. Ideally liquid is added/removed by volume.
+    # the percentage filled after a certain amount of liquid is added/removed
+    # should depend on the volume of the bottle
     def adjust_liquid(self, fill_adjust):
         new_fill_percent = self.percent_filled + fill_adjust
         if  new_fill_percent > 100:
--- fill.py
+++ fill.py
@@ -4,42 +4,62 @@ from pygame.locals import *
 from generate import GenerateTone as generate_tone
 from bottle import Bottle
 
-# set up the colors
-BLACK = (  0,   0,   0)
-WHITE = (255, 255, 255)
-BLUE  = (  0,   0, 255)
+BLACK      = (  0,   0,   0)
+WHITE      = (255, 255, 255)
+BLUE       = (  0,   0, 255)
+PURPLE     = (160,  32, 240)
+GREEN      = (  0, 100,   0)
+YELLOW     = (255, 215,   0)
 
-#TODO: these frequency settings should be derived from the shape and air volume in the bottle
+BOTTLE_BORDER = BLACK
+EMPTY_BOTTLE = (255, 255, 240)
+BACKGROUND   = (205, 200, 177)
+
+#TODO: the frequency should be derived from the shape and air volume in the bottle
 # see: http://physics.stackexchange.com/questions/44601/frequency-of-the-sound-when-blowing-in-a-bottle
 BASE_FREQ = 175
 FREQ_RANGE = 175
 VOLUME = 0.5
 
-def get_tone(fill_percent):
-    new_freq = BASE_FREQ + (FREQ_RANGE * fill_percent / 100)
-    return generate_tone(freq=new_freq, vol=VOLUME)
-
-def draw_bottle(screen, bottle_pos_x, bottle_pos_y, bottle):
-    pg.draw.rect(screen, BLACK, (bottle_pos_x, bottle_pos_y, bottle.width, bottle.height), 1);
-    if bottle.liquid_height() > 0:
-        pg.draw.rect(screen, BLUE, (bottle_pos_x, bottle_pos_y + (bottle.height - bottle.liquid_height()) , bottle.width, bottle.liquid_height()));
-
 def play_bottle_tone(bottle):
-    pg.mixer.Channel(0).queue(get_tone(bottle.percent_filled))
+    freq = BASE_FREQ + (FREQ_RANGE * bottle.percent_filled / 100)
+    pg.mixer.Channel(0).queue(generate_tone(freq=freq, vol=VOLUME))
+
+def render_bottle(screen, position_tuple, bottle):
+    border_width = 1
+    pos_x = position_tuple[0]
+    pos_y = position_tuple[1]
+    pg.draw.rect(screen, EMPTY_BOTTLE, (pos_x, pos_y, bottle.width, bottle.height))
+    pg.draw.rect(screen, BLACK, (pos_x, pos_y, bottle.width, bottle.height), border_width)
+    if bottle.liquid_height > 0:
+        liquid_pos_x = pos_x + border_width
+        liquid_pos_y = pos_y + border_width + bottle.height - bottle.liquid_height
+        liquid_width = bottle.width - border_width * 2
+        liquid_height = bottle.liquid_height - border_width * 2
+        pg.draw.rect(screen, bottle.liquid_color, (liquid_pos_x, liquid_pos_y, liquid_width, liquid_height))
+
+def render(screen, bottles):
+    screen.fill(BACKGROUND)
+    for bottle_key in bottles:
+        render_bottle(screen, bottles[bottle_key]['position'], bottles[bottle_key]['bottle'])
+
+def add_bottle(bottles, position_tuple, bottle):
+    rect = pg.Rect(position_tuple, (bottle.width, bottle.height))
+    bottles[tuple(rect)] = {'bottle': bottle, 'position': position_tuple} #rect in tuple because rect isn't hashable
 
 def main():
     pg.mixer.pre_init(44100, -16, 1, 512)
     pg.init()
 
     # set up the window
-    screen = pg.display.set_mode((600, 450), 0, 32)
+    screen = pg.display.set_mode((650, 300), 0, 32)
     pg.display.set_caption('Bottle Music')
 
-    my_bottle = Bottle(200, 100, 20)
-
-    #bottle position
-    bottle_pos_x = 100
-    bottle_pos_y = 50
+    bottles = {}
+    add_bottle(bottles, (50, 50), Bottle(100, 200, 35, PURPLE))
+    add_bottle(bottles, (200, 50), Bottle(100, 200, 50, BLUE))
+    add_bottle(bottles, (350, 50), Bottle(100, 200, 70, GREEN))
+    add_bottle(bottles, (500, 50), Bottle(100, 200, 100, YELLOW))
 
     fill_rate = 1 # percentage to fill per cycle, range from 1 - 100
 
@@ -64,14 +84,17 @@ def main():
             elif (event.type == KEYUP and event.key == K_b) or event.type == MOUSEBUTTONUP:
                 tone_playing = False
 
-        my_bottle.adjust_liquid(fill_adjust)
+        cursor_rect = pg.Rect(pg.mouse.get_pos(), (0, 0))
+        bottle_keyval = cursor_rect.collidedict(bottles)
 
-        screen.fill(WHITE)
-        draw_bottle(screen, bottle_pos_x, bottle_pos_y, my_bottle)
+        if bottle_keyval is not None:
+            current_bottle = bottle_keyval[1]['bottle']
+            current_bottle.adjust_liquid(fill_adjust)
 
-        if tone_playing:
-            play_bottle_tone(my_bottle)
+            if tone_playing:
+                play_bottle_tone(current_bottle)
 
+        render(screen, bottles)
         pg.display.update()
 
 if __name__ == '__main__': main()
